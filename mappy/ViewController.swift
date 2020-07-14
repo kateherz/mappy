4c9e6562432c93c47310a7b58a512c7b34cb597f
Kate Herz <k8herz4@verizon.net>
2020-07-14 14:06:09 -0400

    Initial Commit

---
+++ mappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mappy
+//
+//  Created by Kate Herz on 7/14/20.
+//  Copyright © 2020 Kate Herz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
