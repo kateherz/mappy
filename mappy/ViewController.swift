295c90860dfb7c4c466af8e37686407ce09fbe31
Kate Herz <k8herz4@verizon.net>
2020-07-15 15:34:54 -0400

    new homepage design, added theme to all winter view controllers

--- mappy/ViewController.swift
+++ mappy/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var startQuizButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startQuizButton.layer.cornerRadius = 20.0
+        startQuizButton.layer.masksToBounds = true
+        
+        
     }
 
 
